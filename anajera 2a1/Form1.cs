d99b440fe8349394075bc26738a2387e2d5aa61a
Angel Najera <oh9355fr@go.minnstate.edu>
2018-10-22 20:31:12 -0500

    after first video

---
+++ anajera 2a1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace anajera_2a1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = Convert.ToDecimal(txtDiscountpercent.Text);
+            decimal discountAmount = subtotal * discountPercent / 100m;
+            txtDiscountamount.Text = discountAmount.ToString("0.00");
+            decimal total = subtotal - discountAmount;
+            txtTotal.Text = total.ToString("0.00");
+        }
+    }
+}
